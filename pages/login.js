7bd09787142d80ff0882f03123abfeeaf6082ef3
nocsg <songgao.cao@foxmail.com>
2022-06-04 14:09:32 +0800

    fix: png

--- pages/login.js
+++ pages/login.js
@@ -2,26 +2,21 @@ import React from 'react';
 import Layout from 'components/layout/Layout';
 import LoginForm from 'components/forms/LoginForm';
 import Back from 'assets/back.svg';
+import styles from './LoginForm.module.css';
+import Icon from 'components/common/Icon';
 
 export default function LoginPage() {
   if (process.env.DISABLE_LOGIN) {
     return null;
   }
 
-  var sectionStyle = {
-    width: '100%',
-
-    height: '400px',
-
-    // makesure here is String确保这里是一个字符串，以下是es6写法
-
-    backgroundImage: `url(${Back})`,
-  };
-
   return (
     <Layout title="login" header={false} footer={false} center>
       <LoginForm />
-      <div style={sectionStyle} />
+      <div className={styles.header}>
+        <Icon icon={<Back />} size="xlarge" className={styles.icon} />
+        <h1 className="center"></h1>
+      </div>
     </Layout>
   );
 }
