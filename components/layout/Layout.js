683dd25028cad0b766d7d95284d7d256d2a5874b
nocsg <songgao.cao@foxmail.com>
2022-06-04 15:14:04 +0800

    del: pic

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,34 +3,21 @@ import Head from 'next/head';
 import Header from 'components/layout/Header';
 import Footer from 'components/layout/Footer';
 import useLocale from 'hooks/useLocale';
-import Back from 'assets/back.svg';
 
 export default function Layout({ title, children, header = true, footer = true, background }) {
   const { dir } = useLocale();
 
-  var sectionStyle = {
-    width: '100%',
-
-    height: '400px',
-
-    // makesure here is String确保这里是一个字符串，以下是es6写法
-
-    backgroundImage: `url(${Back})`,
-  };
-
   return (
     <>
-      <div style={sectionStyle}>
-        <Head>
-          <title>umami{title && ` - ${title}`}</title>
-        </Head>
+      <Head>
+        <title>umami{title && ` - ${title}`}</title>
+      </Head>
 
-        {header && <Header />}
-        <main>{children}</main>
-        {background && <div style={{ backgroundImage: `url(${background})` }} />}
-        {footer && <Footer />}
-        <div id="__modals" dir={dir} />
-      </div>
+      {header && <Header />}
+      <main>{children}</main>
+      {background && <div style={{ backgroundImage: `url(${background})` }} />}
+      {footer && <Footer />}
+      <div id="__modals" dir={dir} />
     </>
   );
 }
