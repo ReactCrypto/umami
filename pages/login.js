30a5f240b1db0635a76d99044756a054b2ca5cc8
nocsg <songgao.cao@foxmail.com>
2022-06-19 17:32:48 +0800

    fix : ui

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from 'components/layout/Layout';
 import LoginForm from 'components/forms/LoginForm';
 import Image from 'next/image';
+import styles from 'components/forms/LoginForm.module.css';
 
 export default function LoginPage() {
   if (process.env.DISABLE_LOGIN) {
@@ -9,7 +10,7 @@ export default function LoginPage() {
   }
 
   return (
-    <Layout title="login" header={false} footer={false} center>
+    <Layout title="login" header={false} footer={false} center className={styles.login}>
       <LoginForm />
       <div style={{ textAlign: 'center', padding: '30px 0px' }}>
         <Image src="/back.jpg" width={6000} height={3375} alt="万兆带宽" />
